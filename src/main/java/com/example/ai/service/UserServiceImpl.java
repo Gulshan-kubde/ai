0c63d125927f4b271d89fbf348ba19b67fba6129
Gulshan-kubde <gulshankubde5222@gmail.com>
2025-11-05 17:31:42 +0530

    datetime

--- src/main/java/com/example/ai/service/UserServiceImpl.java
+++ src/main/java/com/example/ai/service/UserServiceImpl.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -45,6 +47,7 @@ public class UserServiceImpl implements UserService {
                 .mobileNo(request.getMobileNo())
                 .verified(false)
                 .role(userRole)
+                .createdAt(LocalDateTime.now())
                 .build();
 
         User savedUser = userRepository.save(user);
